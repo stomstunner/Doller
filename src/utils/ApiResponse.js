552f61f12ef9ac357c6670c8dcad9ff39bca81c4
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-08-13 18:33:31 +0530

    utils notes

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+// so here we make the api response for the data 
+
+class ApiResponse{
+    constructor(statusCode, data, massage = "Success"){
+        this.statusCode = statusCode
+        this.data = data 
+        this.massage = massage
+        this.success = statusCode < 400
+    }
+}
