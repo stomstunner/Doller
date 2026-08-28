490126cc891125607bad2508a6ff3fde9f054000
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-08-28 17:04:19 +0530

    multer conf

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,20 @@
+import multer from "multer";
+
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    // yaha pe hamre pass request to user se aata hai json me hota hai
+    // but jo ye file hai woh aata hai multer se 
+    // and cb = callback
+  destination: function (req, file, cb) {
+    // Specify your folder path here (make sure the folder exists)
+
+    // and we keep the all file in the public ke ander jo temp hai uske ander 
+    cb(null, './uploads'); 
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
