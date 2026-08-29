3d4cda863a2d2cdc54ba4fd37f1dcd4500dd51ee
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-08-29 17:32:36 +0530

    routes for user register

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,9 +3,36 @@ import { registerUser } from "../controllers/user.controller.js";
 
 const router = Router()
 
+// here we see how to handle the file upload problem 
+// we can easily handle the json data but file uploadation is difficult
+
+import { upload } from "../middlewares/multer.middleware.js";   
+
+
+
+
 // now here we write the router ka kaam ki ham kya kaam kanrege user router se 
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+// so here we write the middleware kyuki ham direct hi register user ko run nahi karwana chahate hai ham chahate hai ki pahale ek middleware ho 
+
+    // so we wanted to send the coverimage and the avatar to by the help of multer jisme ham upload kanrege feields ko as in a array of objects 
+
+    upload.fields(
+        {
+            // first file ka naam // communication bet frontend and backend me help karnege 
+
+            name: "avatar",
+            maxCount : 1
+
+        },
+        {
+            name : "coverImage",
+            maxCount : 1
+        }
+    ),
+    registerUser
+)
 
 // so hamara final url banaega
 // http://localhost:8000/users/register
