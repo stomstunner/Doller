ae9db949f25b7c9110431e2c9d1d9c3b9b09094d
ujjwal kumar <ujjwalk863@gmail.com>
2026-09-15 16:17:03 +0530

    enhance comment model with validation and parent comment support

--- src/models/comment.models.js
+++ src/models/comment.models.js
@@ -5,16 +5,29 @@ const CommentSchema = new Schema(
     {
         content:{
             type: String,
-            required: true
+            required:  [true, "Comment content is required"],
+            trim: true,
+            minlength: [1, "Comment cannot be empty"],
+            maxlength: [1000, "Comment cannot exceed 1000 words"]
         },
         video:{
             type: Schema.Types.ObjectId,
-            ref: "Video"
+            ref: "Video",
+            required: true,
         },
         owner: {
             type: Schema.Types.ObjectId,
-            ref: "User"
+            ref: "User",
+            required: true,
+        },
+        // now  we make a filed for parent comment ager jo kisi se first time khud se commet kiya hai toh uska parent ka vlaue null hoga 
+        // but ager kisi ne pahle se kiye hue commnet pe replay diya hai toh uska parent ka id default me dala jayega
+        parentCommnet: {
+            type: Schema.Types.ObjectId,
+            ref: "Comment",
+            default: null,
         }
+        // 
 
     },{timestamps: true}
 )
