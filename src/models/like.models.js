f1cfb47f7385d933247a9a5c9f6427623f1c280e
ujjwal kumar <ujjwalk863@gmail.com>
2026-09-21 12:49:31 +0530

    feat: add compound unique indexes for like schema to prevent duplicate likes

--- src/models/like.models.js
+++ src/models/like.models.js
@@ -25,4 +25,58 @@ const likeSchema = new Schema(
     },{timestamps: true}
 )
 
+// Like.create() ke duplicate likes se bachne ke liye schema me compound unique index useful hoga.
+
+likeSchema.index(
+    {
+        video: 1,
+        likedBy: 1
+    },
+    {
+        unique: true,
+        partialFilterExpression: {
+            video: { $exists: true }
+        }
+    }
+)
+likeSchema.index(
+    {
+        comment: 1,
+        likedBy: 1
+    },
+    {
+        unique: true,
+        partialFilterExpression: {
+            comment: { $exists: true }
+        }
+    }
+)
+likeSchema.index(
+    {
+        playlist: 1,
+        likedBy: 1
+    },
+    {
+        unique: true,
+        partialFilterExpression: {
+            playlist: { $exists: true }
+        }
+    }
+)
+likeSchema.index(
+    {
+        tweet: 1,
+        likedBy: 1
+    },
+    {
+        unique: true,
+        partialFilterExpression: {
+            tweet: { $exists: true }
+        }
+    }
+)
+
+// so in the partialFilterExpression we set the value uniques to true for tweet if it $exists then it does not store the value 
+
+
 export const Like = new mongoose.model("Like", likeSchema)
