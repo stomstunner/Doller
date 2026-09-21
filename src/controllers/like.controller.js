0466522ce1c037c8c49547ecd49015c82f8f621b
ujjwal kumar <ujjwalk863@gmail.com>
2026-09-21 18:59:44 +0530

    feat: implement getLikedVideos controller with pagination, sorting, and filtering

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -326,3 +326,201 @@ const togglePlaylistLike = asyncHandler(async(req, res) => {
         )
     )
 })
+
+// lets create the getLikedVideos 
+const getLikedVideos = asyncHandler(async(req, res) => {
+    // lets create the important pagination variables 
+    const page = Math.max(
+        Number.parseInt(req.query.page) || 1,
+        1
+    )
+
+    // now we make the limit 
+    const limit = Math.min(
+        Math.max(
+            Number.parseInt(req.query.limit) || 20,
+            1
+        ),
+        100
+    )
+
+    // now we warite the skip variable 
+    const skip = (page - 1) * limit;
+
+    // now we write the search variable 
+    const search = req.query.search?.trim() || "";
+    // ager hamare request ke query me search hai toh usko trim kar ke laao aur search varibale me store kar lo nahi toh usko empty rakho 
+
+    // ab simple sortOptions bana ke rakh lete hai createdAt = -1 jisse hamara videos default  me accesnding order me rakhega 
+
+    const searchOptions = {
+        createdAt : -1
+    }
+
+    // now we make some option for sorting 
+    if(req.query.sortBy === "views"){
+        // then we update the value of the sorting of videos to its views 
+        searchOptions = {
+            views : -1
+        }
+    }
+
+    // for duration 
+    if(req.query.sortBy === "duration"){
+        searchOptions = {
+            duration : -1
+        }
+    }
+
+    // now we write the most important code for the getting the liked videos from the databse based on the aggration pipeline 
+
+    const likedVideos = await Like.aggregate(
+        // aggragete me jata hai array of objects 
+        [
+            // first aggration pipeline 
+            {
+                $match : {
+                    // match kisse karna hai 
+                    likedBy: new mongoose.Types.ObjectId(req.user._id),
+                    video : {
+                        $exists : true
+                    }
+                }
+            },
+            // second pipeline for the joins that ki hame kaha se apne like wale document ko join karna hai jisse hame video ka information mil sake 
+            // look into the video document from the like document 
+
+            {
+                $lookup : {
+                    // for lookup we have the 4 required filds 
+                    from: "videos",
+                    // ye  hamre pass videos model se aaya hai kaha se chaiye hame data 
+                    localField: "video", 
+                    // abhi ham kaha hai 
+                    foreignField: "_id",
+                     // ye hai ki ham jiss document se data chahte hai woha uska kya naam hai 
+                    as: "video",
+
+                    // now we write the code for the pipeline for the level wise aggreation .. nested pipeline 
+
+                    // pipeline me ham array of object 
+                    pipeline: [
+                        // inside the pipeline we write the nested aggregation 
+                        // becase we are in the videos but we only want the videos that match the following  conditon 
+                        {
+                            $match : {
+                                isDeleted : false,
+                                isPublished : true,
+
+                                ...(search && {
+                                    title : {
+                                        $regex : search,
+                                        $options : "i"
+                                    }
+                                })
+                            }
+                        },
+                        // here we write the 2nd aggreation pipeline inside the pileline array of the object 
+                        {
+                            $lookup:{
+                                from : "users",
+                                localField : "owner",
+                                foreignField:  "_id",
+                                as : "owner",
+                                // further we apply the pipeline for the user filed ki hame kya kya filed hi sirf chaiye 
+                                pipeline: [
+                                    // pipeline ka first object 
+                                    {
+                                        // bass hamko dena ya project karna ye kuch field  
+                                        $project:{
+                                            // from the user model
+                                            fullName : 1,
+                                            username: 1,
+                                            avatar : 1
+                                        }
+                                    }
+                                ]
+                            },
+                            // now we add the filed kyuki frontend pe hamare pass array jata hai toh usme se hame first obejct ko nikalna na pare toh ham naye walae owner me object hi rakh denge kyuki pipeline hame array of the obejct deta hai 
+                        },
+                        // 3rd level pipeline aggration
+                        {
+                            $addFields:{
+                                owner : {
+                                    $first : "$owner",
+                                }
+                            }
+                        },
+                        // now we apply the 4th level pipeline for projection of the video ki hame videos me se kya kya data chaiye in the frontend we only write the some field 
+                        {
+                            $project:{
+                                // here we write the field that we want in the frontend 
+                                title : 1,
+                                description: 1,
+                                thumbnail : 1,
+                                duration : 1,
+                                views : 1,
+                                owner : 1,
+                                createdAt : 1,
+
+                            }
+                        }
+
+
+                    ]
+                }
+            },
+            // this is the 3 level parent level pipeline wher we apply the sorting adn the limit aggration and the some important filter that is  needed for the pagination 
+            {
+                // this is for the chenging the array to the obejct lookup se hamare jitne bhi videos hai woh array me aayenge but unwind se woh object me badal jayega jisse unko acces karna easy hoga 
+                $unwind : "$video"
+            },
+            {
+                $replaceRoot: {
+                    newRoot: "$video"
+                }
+            },
+            // now we apply the sort 
+            {
+                $sort : sortoptions
+            },
+            // now we write the skip for the pagination 
+            {
+                $skip : skip
+            },
+            {
+                $limit : limit
+            }
+        ]
+    );
+
+    // now we find the total count of the how many we have the videos that we like them 
+    const totalLikedVideos = await Like.countDocuments(
+        {
+            likedBy: req.user._id,
+            video: {
+                $exists: true
+            }
+        }
+    );
+
+    // now we send the response of the likedVideos 
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                videos : likedVideos,
+                page,
+                limt,
+                totalLikedVideos,
+                totalPages : Math.ceil(
+                    totalLikedVideos / limit
+                ) ,
+                hasNextPage : (page * limit) < totalLikedVideos
+            },
+            "Liked Videos Fetched Successfully"
+        )
+    )
+})
