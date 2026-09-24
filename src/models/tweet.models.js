5f5358338488c709d21544d572e64b4b781a4551
ujjwal kumar <ujjwalk863@gmail.com>
2026-09-24 18:05:44 +0530

    feat: enhance tweet schema with additional fields for deletion, editing, and reply count

--- src/models/tweet.models.js
+++ src/models/tweet.models.js
@@ -4,10 +4,31 @@ const tweetSchema = new Schema({
     content: {
         type: String,
         required: true,
+        trim: true,
+        minLength: [1, "Tweet cannot be empty"],
+        maxLength: [300, "Tweet cannot exceeded 300 characters"]
     },
     owner:{
         type: Schema.Types.ObjectId,
-        ref:"User"
+        ref:"User",
+        required: true,
+    },
+    isDeleted : {
+        type: Boolean,
+        default: false,
+    },
+    isEdited: {
+        type: Boolean,
+        default: false,
+    },
+    editedAt: {
+        type: Date,
+        default: null,
+    },
+    replyCount: {
+        type: Number,
+        default: 0,
+        min: 0
     }
 },{timestamps: true})
 
